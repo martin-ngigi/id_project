86e89dddfa8f10d9ad1145f6fc3662e7f6796202
martin-ngigi <martinwainaina001@gmail.com>
2023-01-12 18:15:20 +0300

    Added lists

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
       MaterialApp(
-          home: Home()
+          home: Quotelist()
       )
   );
 }
@@ -11,114 +11,35 @@ void main() {
 //short-cut for StatelessWidget is "stless"
 
 //short-cut for StatefulWidget is "stful"
-class Home extends StatefulWidget {
+class Quotelist extends StatefulWidget {
   @override
-  State<Home> createState() => _HomeState();
+  State<Quotelist> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Quotelist> {
 
   int level = 0;
 
   @override
   Widget build(BuildContext context) {
+    List<String> quotes_list = [
+      "Be yourself, everyone else is already taken.",
+      "I have nothing else to declare except my genius.",
+      "The truth is rarely pure and never simple."
+    ];
+
     //Scaffold allows us to create a well layered app with appbar, body
     return Scaffold(
-      backgroundColor: Colors.grey[900], //entire background
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text("World Timer App"),
+        title: Text("Quote List app"),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0, //remove shadow
+        backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // horizontal alignment.
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/image1.jpg"),
-                radius: 50.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[400],
-            ),
-            Text(
-              "Name",
-              style: TextStyle(
-                  color: Colors.grey[400],
-                  letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 10.0), //for placing a gap in between the fields.
-            Text(
-              "Martin",
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10.0), //
-            Text(
-              "Current Dev level",
-              style: TextStyle(
-                  color: Colors.grey[400],
-                  letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 10.0), //for placing a gap in between the fields.
-            Text(
-              "${level}",
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10.0), //for placing a gap in between the fields.
-            Row(
-              children: [
-                Icon(
-                  Icons.mail,
-                  color: Colors.grey[400],
-                  size: 24.0,
-                  // semanticLabel: 'Email',
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 0, 0), //left, Top, Right, Bottom
-                  //instead of a container, one can also use sized box
-                  child: Text(
-                    "martinwainaina001@gmail.com",
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      letterSpacing: 1.0,
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[400],
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            level += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[700],
+      body: Column(
+        children: quotes_list.map((quote)
+        => Text(quote) // "=>" is used as a return statement.
+        ).toList(),
       ),
     );
   }
